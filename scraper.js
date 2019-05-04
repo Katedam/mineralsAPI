0578b793247f301bdc547c803da826e5a1389f9b
Kate Dameron <katerj33@gmail.com>
2019-05-04 15:11:10 -0700

    WIP troubleshooting scraping background image urls and mineral names

--- scraper.js
+++ scraper.js
@@ -1,9 +1,12 @@
 const request = require('superagent');
 const { parse } = require('node-html-parser');
 
-module.exports = () => {
+const getMinerals = () => {
   return request.get('https://mineralseducationcoalition.org/mining-minerals-information/minerals-database/?tax=mineral-category')
     .then(res => res.text)
     .then(parse)
-    .then(html => console.log(html));
+    .then(html => html.querySelectorAll('.minerals-article__title'))
+    .then(res => console.log(res));
 };
+
+getMinerals();
