b9d15c5d73df227cabe621c4e813b759752219f9
Kate Dameron <katerj33@gmail.com>
2019-05-07 10:52:41 -0700

    troubleshooting tricky data

--- scraper.js
+++ scraper.js
@@ -2,35 +2,38 @@ const request = require('superagent');
 const { parse } = require('node-html-parser');
 const { mineralsList } = require('./lib/services/mineralsList');
 
-const getDetails = (html) => {
+const getDetails = (html, mineral) => {
   let pElements = html.querySelectorAll('p');
   let description = pElements[1].rawText;
   let images = html.querySelectorAll('.acf-image-gallery__image.mec-share-image').map(el => el.rawAttrs);
-  let type = pElements[2].rawText;
-  let classification = pElements[3].rawText;
-  let chemicalFormula = pElements[4].rawText;
-  let streak = pElements[5].rawText;
-  let mohsHardness = pElements[6].rawText;
-  let crystalSystem = pElements[7].rawText;
-  let color = pElements[9].rawText;
-  let luster = pElements[10].rawText;
-  let fracture = pElements[11].rawText;
+  let details = html.querySelectorAll('.mec-db-entry__details-grid-cell').map(html => html.querySelectorAll('p'));
+  console.log(details);
+  // let type = pElements[2].rawText;
+  // let classification = pElements[3].rawText;
+  // let chemicalFormula = pElements[4].rawText;
+  // let streak = pElements[5].rawText;
+  // let mohsHardness = pElements[6].rawText;
+  // let crystalSystem = pElements[7].rawText;
+  // let color = pElements[9].rawText;
+  // let luster = pElements[10].rawText;
+  // let fracture = pElements[11].rawText;
 
   const mineralDetails = {
+    mineral: mineral,
     description,
-    images,
-    type,
-    classification,
-    chemicalFormula,
-    streak,
-    mohsHardness,
-    crystalSystem,
-    color,
-    luster,
-    fracture
+    images
+    // type,
+    // classification,
+    // chemicalFormula,
+    // streak,
+    // mohsHardness,
+    // crystalSystem,
+    // color,
+    // luster,
+    // fracture
   };
 
-  console.log(mineralDetails);
+  // console.log(mineralDetails);
 };
 
 const getMinerals = () => {
@@ -38,7 +41,7 @@ const getMinerals = () => {
     return request.get(`https://mineralseducationcoalition.org/minerals-database/${mineral}`)
       .then(res => res.text)
       .then(parse)
-      .then(getDetails)
+      .then(html => getDetails(html, mineral))
       .catch(console.error());
   });
 };
