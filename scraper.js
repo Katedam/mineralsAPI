132d16470cd24fe8f0627e10a7518b68022ec312
Kate Dameron <katerj33@gmail.com>
2019-05-06 17:36:19 -0700

    scraping minerals details but some are coming back wonky

--- scraper.js
+++ scraper.js
@@ -1,12 +1,48 @@
 const request = require('superagent');
 const { parse } = require('node-html-parser');
+const { mineralsList } = require('./lib/services/mineralsList');
+
+const getDetails = (html) => {
+  let pElements = html.querySelectorAll('p');
+  let description = pElements[1].rawText;
+  let images = html.querySelectorAll('.acf-image-gallery__image.mec-share-image').map(el => el.rawAttrs);
+  let type = pElements[2].rawText;
+  let classification = pElements[3].rawText;
+  let chemicalFormula = pElements[4].rawText;
+  let streak = pElements[5].rawText;
+  let mohsHardness = pElements[6].rawText;
+  let crystalSystem = pElements[7].rawText;
+  let color = pElements[9].rawText;
+  let luster = pElements[10].rawText;
+  let fracture = pElements[11].rawText;
+
+  const mineralDetails = {
+    description,
+    images,
+    type,
+    classification,
+    chemicalFormula,
+    streak,
+    mohsHardness,
+    crystalSystem,
+    color,
+    luster,
+    fracture
+  };
+
+  console.log(mineralDetails);
+};
 
 const getMinerals = () => {
-  return request.get('https://mineralseducationcoalition.org/mining-minerals-information/minerals-database/?tax=mineral-category')
-    .then(res => res.text)
-    .then(parse)
-    .then(html => html.querySelectorAll('.minerals-article__title'))
-    .then(res => console.log(res));
+  mineralsList.map(mineral => {
+    return request.get(`https://mineralseducationcoalition.org/minerals-database/${mineral}`)
+      .then(res => res.text)
+      .then(parse)
+      .then(getDetails)
+      .catch(console.error());
+  });
 };
 
 getMinerals();
+
+
